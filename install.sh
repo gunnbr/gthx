7488cc6f674280ba3a19bfe8950cb3c944050598
Brian Gunn <github@gunnbr.org>
2017-02-19 16:33:46 -0800

    Switched to using systemd scripts instead of upstart to get this to work on Ubuntu 16.04

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-sudo cp gthx-upstart.local /etc/init/gthx.conf
+echo -n Stopping gthx service...
+sudo systemctl stop gthx
+echo Done.
+sudo cp gthx.service /lib/systemd/system/gthx.service
+sudo cp gthx-environment.local /etc/default/gthx
+sudo systemctl daemon-reload
 sudo mkdir -p /usr/sbin/gthx
 sudo cp gthx.py /usr/sbin/gthx/
 sudo cp DbAccess.py /usr/sbin/gthx/
 sudo cp Email.py /usr/sbin/gthx/
+echo -n Starting gthx service...
+sudo systemctl start gthx
+echo Done.
