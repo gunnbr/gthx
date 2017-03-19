ac2d7c98a4d14a25c996d27fbd1055d241242766
Brian Gunn <github@gunnbr.org>
2017-03-18 21:19:13 -0700

    Fixed permissions and security for gthx.config file.

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ echo Done.
 sudo cp gthx.service /lib/systemd/system/gthx.service
 sudo mkdir -p /etc/gthx
 sudo cp gthx.config.local /etc/gthx/gthx.config
-sudo chmod a-r /etc/gthx/gthx.config
+sudo chown www-data /etc/gthx/gthx.config
+sudo chgrp www-data /etc/gthx/gthx.config
+sudo chmod 640 /etc/gthx/gthx.config
 sudo systemctl daemon-reload
 sudo mkdir -p /usr/sbin/gthx
 sudo cp gthx.py /usr/sbin/gthx/
